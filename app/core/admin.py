cb8b4c8e9123c88049af2561cafba870ff8be778
Sohet <maxime.sohet@b12-consulting.com>
2020-07-31 11:55:16 +0200

    Configure postgres db.

--- app/core/admin.py
+++ app/core/admin.py
@@ -26,7 +26,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
              'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2')
+             'fields': ('email', 'password1', 'password2')
         }),
     )
 
